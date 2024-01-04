8218edced130750f4b610af9b0ea6cb60d3a6496
JoseAConcepcion <concepcijoseantonio@gmail.com>
2024-01-04 17:56:13 -0500

    feat: scroll and color change

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 window.addEventListener('scroll', function() {
-  let elemento = document.querySelector('.desarrollo');
+  // let elemento = document.querySelector('.desarrollo');
+  let elemento2 = document.querySelector('.contenido');
+  let imagen = document.querySelector('.imagen img');
   let scrollActual = window.scrollY;
 
-  if (scrollActual > 200) { // Cambia el color después de cierto desplazamiento
-    elemento.classList.add('cambio-color');
+  if (scrollActual > 20) {
+    let colorIndex = Math.floor(scrollActual / 1000); // Calcula el índice del color cada 100px
+    let colors = ['#FFC0CB', '#c0ffc4', 'blue', 'yellow', 'green', 'red', 'orange', '#ABCDEF', 'purple', '#FA8072', '#90EE90', '#FFD700', '#7B68EE', '#00CED1', '#FF6347']; // Colores adicionales
+    let color = colors[colorIndex % colors.length]; // Obtiene un color de la lista de colores
+
+    // elemento2.style.backgroundColor = color;
+    // elemento.style.backgroundColor = color;
+    imagen.style.filter = `hue-rotate(${colorIndex * 30}deg)`;
   } else {
-    elemento.classList.remove('cambio-color');
+    // elemento.style.backgroundColor = ''; // Restaura el color por defecto
+    // elemento2.style.backgroundColor = ''; // Restaura el color por defecto
   }
 });
+
+
+// window.addEventListener('scroll', function() {
+//   let elemento = document.querySelector('.desarrollo');
+//   let elemento2 = document.querySelector('.contenido');
+//   let scrollActual = window.scrollY;
+
+//   if (scrollActual > 20) { // Cambia el color después de cierto desplazamiento
+//     elemento2.classList.add('cambio-color');
+//     elemento.classList.add('cambio-color');
+//   } else {
+//     elemento.classList.remove('cambio-color');
+//     elemento2.classList.remove('cambio-color');
+//   }
+// });
