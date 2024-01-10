8644707c12fde0b2c831ca9e740b106cc8bf4c08
JoseAConcepcion <concepcijoseantonio@gmail.com>
2024-01-10 04:52:53 -0500

    feat: data and a simple table to display colors

--- script.js
+++ script.js
@@ -18,17 +18,47 @@ window.addEventListener('scroll', function() {
   }
 });
 
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        const tableContainer = document.getElementById('table-container');
+        
+        // Lógica para crear la tabla utilizando los datos del JSON
+        const colorTable = createColorTable(data);
+        tableContainer.appendChild(colorTable);
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo JSON:', error);
+    });
 
-// window.addEventListener('scroll', function() {
-//   let elemento = document.querySelector('.desarrollo');
-//   let elemento2 = document.querySelector('.contenido');
-//   let scrollActual = window.scrollY;
+function createColorTable(data) {
+    const table = document.createElement('table');
+    table.border = '2';
 
-//   if (scrollActual > 20) { // Cambia el color después de cierto desplazamiento
-//     elemento2.classList.add('cambio-color');
-//     elemento.classList.add('cambio-color');
-//   } else {
-//     elemento.classList.remove('cambio-color');
-//     elemento2.classList.remove('cambio-color');
-//   }
-// });
+    // Recorrer el JSON para extraer los colores
+    for (const year in data) {
+        for (const month in data[year]) {
+            for (const day in data[year][month]) {
+                const dayData = data[year][month][day];
+                const dayColors = dayData.map(item => item.hex).flat(); // Obtiene todos los colores de los elementos del día
+
+                // Crear una fila por cada conjunto de colores
+                const row = table.insertRow();
+                const cellDay = row.insertCell(0);
+                cellDay.innerHTML = `${year}-${month}-${day}`;
+
+                const cellColors = row.insertCell(1);
+                for (const color of dayColors) {
+                    const colorBox = document.createElement('div');
+                    colorBox.style.backgroundColor = color;
+                    colorBox.style.width = '30px';
+                    colorBox.style.height = '30px';
+                    colorBox.style.marginRight = '5px';
+                    cellColors.appendChild(colorBox);
+                }
+            }
+        }
+    }
+
+    return table;
+}
